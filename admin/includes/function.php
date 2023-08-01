e7e3727afe806187d40df7b1632c1edb88b97263
EvaristusTech <arinzeevaristus640@gmail.com>
2023-08-01 22:02:50 +0100

    Validating the forms

--- admin/includes/function.php
+++ admin/includes/function.php
@@ -268,24 +268,6 @@ function register_user($username, $email, $password) {
 
      global $connection;
 
-      $username = $_POST['username'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-
-
-    if (username_exists($username)) {
-        // code...
-        $message = "Username exist!!!";
-
-    } elseif (email_exists($email)) {
-
-        $message = "Email exist!!!";
-
-    }
-
-    elseif (!empty($username) && !empty($email) && !empty($password)) {
-        // code...
-
     $username = mysqli_real_escape_string($connection, $username);   
     $email = mysqli_real_escape_string($connection, $email);   
     $password = mysqli_real_escape_string($connection, $password);
@@ -314,16 +296,55 @@ function register_user($username, $email, $password) {
         $register_user_query = mysqli_query($connection, $query);
         if (!$register_user_query) {
             // code...
-            die("Query Failed" . mysqli_error($connection) . ' ' . mysqli_errno($connection));
+            die("Query Failed" . mysqli_error($connection) . ' ' . mysqli_error($connection));
         }
 
-        $message = "Your Registration Has Been Submitted";
 
     }
-     else {
-        $message = "Field Can Not Be Empty";
+   
+
+
+
+function login_user($username, $password) {
+
+    global $connection;
+
+    $username = mysqli_real_escape_string($connection, $username);
+    $password = mysqli_real_escape_string($connection, $password);
+
+
+    $query = "SELECT * FROM users WHERE username = '{$username}' ";
+    $select_user_query = mysqli_query($connection, $query);
+
+    if (!$select_user_query) {
+        // code...
+        die("Query Failed" . mysqli_error($connection));
     }
 
-} else {
-    $message = "";
+    while ($row = mysqli_fetch_assoc($select_user_query)) {
+        // code...
+        $db_user_id = $row['user_id'];
+        $db_username = $row['username'];
+        $db_user_firstname = $row['user_firstname'];
+        $db_user_lastname = $row['user_lastname'];
+        $db_user_role = $row['user_role'];
+        $db_user_password = $row['user_password'];
+    }
+
+        // $password = crypt($password, $db_user_password);
+    
+
+if (password_verify($password, $db_user_password)){
+        // code...
+
+        $_SESSION['username'] = $db_username;
+        $_SESSION['firstname'] = $db_user_firstname;
+        $_SESSION['lastname'] = $db_user_lastname;
+        $_SESSION['user_role'] = $db_user_role;
+
+        header("Location: ../admin");
+    }else {
+        header("Location: ../index.php");
+    }
+
 }
--- registration.php
+++ registration.php
@@ -4,7 +4,7 @@
 <?php
 
 
-if (isset($_POST['submit'])) {
+if ($_SERVER['REQUEST_METHOD'] == "POST ") {
     // code...
 
     $username = trim($_POST['username']);
@@ -18,7 +18,7 @@ if (isset($_POST['submit'])) {
         'password' => ''
     ];
    
-   if (strlen($username) < 4) {
+   if (strlen($username) < 3) {
         $error['username'] = 'Username too short!!!';
    }
    
@@ -26,30 +26,33 @@ if (isset($_POST['submit'])) {
         $error['username'] = 'Username cannot be empty!!!';
    }
 
-    if (username_exists($username)) {
+     if (username_exists($username)) {
         $error['username'] = 'Username already exist!!!';
    }
 
-   if ($email = '') {
+    if ($email = '') {
         $error['email'] = 'email cannot be empty!!!';
    }
 
-    if (email_exists($email)) {
+     if (email_exists($email)) {
         $error['email'] = 'email already exist!!!';
    }
 
-   if ($password = '') {
+    if ($password = '') {
         $error['password'] = 'password cannot be empty!!!';
    }
 
    foreach ($error as $key => $value) {
        // code...
     if (empty($value)) {
-        // code...
-        register_user($username, $email, $password);
-
-        login_user($username, $password)
+      
+      unset($error[$key]);
     }
+   }//foreach loop
+
+   if (empty($error)) {
+       // code...
+        register_user($username, $email, $password);
    }
 
 }
@@ -78,18 +81,21 @@ if (isset($_POST['submit'])) {
 
                         <div class="form-group">
                             <label for="username" class="sr-only">username</label>
-                            <input type="text" name="username" id="username" class="form-control" placeholder="Enter Desired Username">
+                            <input type="text" name="username" id="username" class="form-control" placeholder="Enter Desired Username" autocomplete="on" value="<?php echo isset($username) ? $username : '' ?>" >
+                            <p><?php echo isset($error['username']) ? $error['username'] : ''; ?></p>
                         </div>
                          <div class="form-group">
                             <label for="email" class="sr-only">Email</label>
-                            <input type="email" name="email" id="email" class="form-control" placeholder="somebody@example.com">
+                            <input type="email" name="email" id="email" class="form-control" placeholder="somebody@example.com" autocomplete="on" value="<?php echo isset($email) ? $email : '' ?>">
+                            <p><?php echo isset($error['email']) ? $error['email'] : '' ;?></p>
                         </div>
                          <div class="form-group">
                             <label for="password" class="sr-only">Password</label>
                             <input type="password" name="password" id="key" class="form-control" placeholder="Password">
+                            <p><?php echo isset($error['password']) ? $error['password'] : '' ;?></p>
                         </div>
                 
-                        <input type="submit" name="submit" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Register">
+                        <input type="submit" name="Register" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Register">
                     </form>
                  
                 </div>
