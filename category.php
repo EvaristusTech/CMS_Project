67e21a7ef6dcb14d336fe718c4eb26362ba52741
EvaristusTech <arinzeevaristus640@gmail.com>
2023-08-04 22:56:30 +0100

    adding a prepare statment to our cms system

--- category.php
+++ category.php
@@ -34,10 +34,12 @@
 
     if (is_admin($_SESSION['username'])) {
         // code...
-           $query1 = mysqli_prepare($connection, "SELECT post_id, post_title, post_auther, post_date, post_image, post_content FROM posts WHERE post_catetory_id = ?");
+            $var1 = "SELECT post_id, post_title, post_auther, post_date, post_image, post_content FROM posts WHERE post_catetory_id = ?";
+           $query1 = mysqli_prepare($connection, $var1);
     } else {
 
-        $query2 = mysqli_prepare($connection, "SELECT post_id, post_title, post_auther, post_date, post_image, post_content FROM posts WHERE post_catetory_id = ? AND post_status = ? "); 
+        $var2 = "SELECT post_id, post_title, post_auther, post_date, post_image, post_content FROM posts WHERE post_catetory_id = ? AND post_status = ? ";
+        $query2 = mysqli_prepare($connection, $var2); 
 
         $published = 'published';
     }
@@ -47,32 +49,47 @@
 
     if (isset($query1)) {
         // code...
-        // mysqli_stmt_bind_param();
+       mysqli_stmt_bind_param($query1, "i", $post_catetory_id);
 
+       mysqli_stmt_excute($query1);
 
+       mysqli_stmt_bind_result($query1, $post_id, $post_title, $post_auther, $post_date, $post_image, $post_content);
+
+       $query = $query1
+
+    } else {
+
+        mysqli_stmt_bind_param($query2, "is", $post_catetory_id, $published);
+
+       mysqli_stmt_excute($query2);
+
+       mysqli_stmt_bind_result($query2, $post_id, $post_title, $post_auther, $post_date, $post_image, $post_content);
+
+        $query = $query2
 
     }
 
 
 
-        if (mysqli_num_rows($post_item) < 1) {
+        if (mysqli_stmt_num_rows($query) === 0) {
             // code...
 
             echo "<h1 class='text-center'>No Post Avaliable</h1>";
-        } else{
+        } 
+        // else{
 
-        while ($row = mysqli_fetch_assoc($post_item)) {
+        while (mysqli_stmt_fetch($query)):
             // code...
-            $post_id = $row['post_id'];
-            $post_catetory_id = $row['post_catetory_id'];
-            $post_title = strtoupper($row['post_title']);
-            $post_author = $row['post_author'];
-            $post_date = $row['post_date'];
-            $post_image = $row['post_image'];
-            $post_content = substr($row['post_content'], 0, 200);
-            $post_tags = $row['post_tags'];
-            $post_comment_count = $row['post_comment_count'];
-            $post_status = $row['post_status'];
+            // $post_id = $row['post_id'];
+            // $post_catetory_id = $row['post_catetory_id'];
+            // $post_title = strtoupper($row['post_title']);
+            // $post_author = $row['post_author'];
+            // $post_date = $row['post_date'];
+            // $post_image = $row['post_image'];
+            // $post_content = substr($row['post_content'], 0, 200);
+            // $post_tags = $row['post_tags'];
+            // $post_comment_count = $row['post_comment_count'];
+            // $post_status = $row['post_status'];
        
 
 
@@ -109,10 +126,9 @@
 
                 
 
-<?php
+<?php endwhile;
     }
-        }
-            }else{
+        else{
             header("location: index.php");
         }
 
